8a77285d88b4c0dc4ea67860369550cb290ebcec
YashKachaRK <ykacha243@rku.ac.in>
2025-12-20 17:29:42 +0530

    first commit

---
+++ PORTFOLIO/src/App.jsx
@@ -0,0 +1,49 @@
+
+import ToDo from "./ToDo";
+
+// function App(){
+//   const name ="Yash kacha";
+//   return(
+//     // <div>
+//     //   {/* <h1>First</h1>
+//     //   <App2></App2>
+//     //   <Login></Login> */}
+
+
+//     //   <button onClick={()=>alert("Hello")}>
+//     //     Click
+//     //   </button>
+
+      
+//     // </div>
+//     // <div>
+//     //     <ToDo/>
+//     // </div>
+
+
+//       <div>
+
+//       </div>
+//   )
+// }
+
+
+
+// function App2(){
+//   return(
+//     <h2>Yash kacha</h2>
+//   )
+// }
+
+
+function App(){
+  const name = "Yash kacha";
+
+  return(
+    <div>
+      {name}
+    </div>
+  )
+}
+
+export default App;
---
+++ PORTFOLIO/src/USer.jsx
@@ -0,0 +1,20 @@
+export function Login(){
+    return(
+        <div>
+            <h1>
+                login
+            </h1>
+            <Userk/>
+        </div>
+    )
+}
+
+function Userk(){
+    return(
+        <div>
+            <h1> User is kacha</h1>
+        </div>
+    )
+}
+
+// export default  Login;
