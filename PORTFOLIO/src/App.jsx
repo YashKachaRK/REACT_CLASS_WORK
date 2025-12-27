85b87ea9c47d89b6c751aec4bf2e84eff85afaa0
YashKachaRK <ykacha243@rku.ac.in>
2025-12-27 20:19:56 +0530

    nothing

--- PORTFOLIO/src/App.jsx
+++ PORTFOLIO/src/App.jsx
@@ -1,66 +1,19 @@
 
-import ToDo from "./ToDo";
-import T from "./TASK1";
-import MyComponent  from "./Components";
+import MyComponent from "./Components";
 import Props from "./Props";
-// function App(){
-//   const name ="Yash kacha";
-//   return(
-//     // <div>
-//     //   {/* <h1>First</h1>
-//     //   <App2></App2>
-//     //   <Login></Login> */}
 
-
-//     //   <button onClick={()=>alert("Hello")}>
-//     //     Click
-//     //   </button>
-
-      
-//     // </div>
-//     // <div>
-//     //     <ToDo/>
-//     // </div>
-
-
-//       <div>
-
-//       </div>
-//   )
-// }
-
-
-
-// function App2(){
-//   return(
-//     <h2>Yash kacha</h2>
-//   )
-// }
-
-
-// function App(){
-//   const name = "Yash kacha";
-
-//   return(
-//     <div>
-//       {name}
-//       <ToDo/>
-//     </div>
-//   )
-// }
-
-function App(){
-  const BtnClicked = () =>{
+function App() {
+  const BtnClicked = () => {
     alert("Button Clicked");
-  }
-  return(
+  };
+  return (
     <div>
-      {/* <MyComponent/> */}
+      <MyComponent />
 
-      <Props name = "Yash Kacha" BtnClicked = {BtnClicked}/>
-      
+      {/* <Props name="Yash Kacha" BtnClicked={BtnClicked} /> */}
+
     </div>
-  )
+  );
 }
 
 export default App;
